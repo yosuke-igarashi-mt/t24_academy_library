b65c439d94e7a1e826f7b73438c671077b3708e7
miku-wakabayashi <49266428+miku-wakabayashi@users.noreply.github.com>
2024-04-12 16:31:17 +0900

    Update create_table.sql
    貸出テーブルCreate削除

--- docker/mysql/initdb/create_table.sql
+++ docker/mysql/initdb/create_table.sql
@@ -46,23 +46,3 @@ CREATE TABLE IF NOT EXISTS mt_library.stocks
 )
 COMMENT = '在庫'
 ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS mt_library.rental_manage
-(
-    id bigint NOT NULL AUTO_INCREMENT comment '貸出管理番号',
-    stock_id varchar(20) NOT NULL comment '在庫管理番号',
-    employee_id varchar(50) NOT NULL comment '社員番号',
-    status tinyint NOT NULL comment '貸出ステータス',
-    expected_rental_on date comment '貸出予定日',
-    expected_return_on date comment '返却予定日',
-    rentaled_at datetime comment '貸出日時',
-    returned_at datetime comment '返却日時',
-    canceled_at datetime comment 'キャンセル日時',
-    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    FOREIGN KEY fk_stock_id (stock_id) REFERENCES stocks (id),
-    FOREIGN KEY fk_employee_id (employee_id) REFERENCES accounts (employee_id)
-)
-COMMENT = '貸出管理'
-ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
